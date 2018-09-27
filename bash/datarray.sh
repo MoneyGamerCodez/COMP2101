f20a7e98b294c899981687dc95d11253c1f6e698
MoneyGamerCodez <200343345@student.georgianc.on.ca>
2018-09-27 10:28:30 -0400

    testing and arrays

--- bash/datarray.sh
+++ bash/datarray.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ###########################################
-#					  #
+#					                                #
 # Creating the arrays that will store 	  #
 # the color with the corrisponding animal #
-#					  #
+#					                                #
 ###########################################
 
 declare -A animals
@@ -12,15 +12,14 @@ animals=(["red"]="cardinal" ["green"]="turtle" ["blue"]="whale" ["white"]="chick
 
 #####################################################################
 # Ask the user to pick a number between 1 and the number of colours #
-# present in the Array						    #
+# present in the Array						                                  #
 #####################################################################
 
 read -p "Give me a number from 1-${#colours[@]}: " number
 
 #########################################################
 # The next line/function will read out the users number #
-# along with the colour and creature 			#
+# along with the colour and creature 			              #
 #########################################################
 
 echo "You picked $number, which is the $colour $animal"
-
